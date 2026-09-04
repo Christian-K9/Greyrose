d46431921d9bf502c7a5a31635dd681012e82385
Christian-K9 <cmkent2005@gmail.com>
2026-09-04 16:56:21 -0400

    Fixed forwarder problem

--- linux/setup.py
+++ linux/setup.py
@@ -1,90 +1,102 @@
 import subprocess
 import logging
 
-#types of servers running
-machines = {0: "Debian", 1: "Ubuntu", 2: "centOS"
-            3: "Fedora"}
-print("Server Types:")
-print(machines)
-
-#machine is based on user input
-server = input("What Server Are You Running? (Enter Number): ")
-
 #assign splunk forwarder based on machine
-forwarders = {debian: "https://download.splunk.com/products/universalforwarder/#releases/10.0.1/linux/splunkforwarder-10.0.1-c486717c322b-linux-amd64.deb",
-            ubuntu: "https://download.splunk.com/products/universalforwarder/#releases/10.0.1/linux/splunkforwarder-10.0.1-c486717c322b-linux-amd64.deb",
-            centos: "https://download.splunk.com/products/universalforwarder/#releases/10.0.1/linux/splunkforwarder-10.0.1-c486717c322b-linux-amd64.deb",
-            fedora: "https://download.splunk.com/products/universalforwarder/releases/#10.0.1/linux/splunkforwarder-10.0.1-c486717c322b.x86_64.rpm"
-}
+forwarders = {"debian": "https://download.splunk.com/products/universalforwarder/releases/10.0.3/linux/splunkforwarder-10.0.3-adbac1c8811c-linux-amd64.deb",
+    "ubuntu": "https://download.splunk.com/products/universalforwarder/releases/10.0.3/linux/splunkforwarder-10.0.3-adbac1c8811c-linux-amd64.deb",
+    "centos": "https://download.splunk.com/products/universalforwarder/releases/10.0.3/linux/splunkforwarder-10.0.3-adbac1c8811c-linux-amd64.deb",
+    "fedora": "https://download.splunk.com/products/universalforwarder/releases/10.0.3/linux/splunkforwarder-10.0.3-adbac1c8811c-linux-amd64.rpm"
+    }
 
-log_name = subprocess.run(["sudo", "grep", "-Po", "'^NAME="\K[^"]+' /etc/os-release"])
-#Centralized logging in ubuntu.log file
-basicConfic(level=logging.DEBUG, filename=log_name, 
-    filemode="w", format="%(asctime)s - %(levelname)s - %(message)s")
+#types of servers running
+machines = ["debian", "ubuntu", "centos", "fedora"]
+print("Server Types:")
+print(f"        {machines}")
 
-#run nftables based on loaded configuration file
-print("Running nftables...")
-subprocess.run(["sudo", "nft", 
-    "-f", "nftables.conf"])
-logging.debug("Running nftables")
+server = ""
+#machine is based on user input
+while server not in machines:
+    server = input("What Server Are You Running? ")
+    if server.lower() not in machines:
+        print("Not Valid Operating System Name")
 
-#change the permission of every file in the directory
-print("Applying permissions...")
-subprocess.run(["sudo", "chmod", "700", "setup.py"])
-subprocess.run(["sudo", "chmod", "700", "start.py"])
-subprocess.run(["sudo", "chmod", "700", "tracker.py"])
-logging.debug("Applying permissions to files inside Greyrose Directory")
+def act_I():
+    log_name = server
+    print(f"Log Name: {log_name}")
+    #Centralized logging in ubuntu.log file
+    logging.basicConfig(level=logging.DEBUG, filename=log_name, 
+        filemode="w", format="%(asctime)s - %(levelname)s - %(message)s")
 
-#start the nftables service
-print("Starting nftables service...")
-subprocess.run(["sudo", "systemctl", "start", "nftables"])
-logging.debug("Starting nftable service")
+def act_II():
+    #run nftables based on loaded configuration file
+    print("Running nftables...")
+    subprocess.run(["sudo", "nft", 
+        "-f", "nftables.conf"])
+    logging.debug("Running nftables")
 
-#enable the nftables service
-print("Enabling nftables service...")
-subprocess.run(["sudo", "systemctl", "enable", "--now", "nftables"])
-logging.debug("Enabling nftables service")
+    #start the nftables service
+    print("Starting nftables service...")
+    subprocess.run(["sudo", "systemctl", "start", "nftables"])
+    logging.debug("Starting nftable service")
 
-#reloading changes based on conf table
-subprocess.run(["sudo", "nft", "nftables.conf"])
+    #enable the nftables service
+    print("Enabling nftables service...")
+    subprocess.run(["sudo", "systemctl", "enable", "--now", "nftables"])
+    logging.debug("Enabling nftables service")
 
-#create service
-subprocess.run(["cp", "Greyrose.service", "etc/systemd/system/Greyrose.service"])
-subprocess.run(["sudo", "systemctl", "daemon-reload"])
-subprocess.run(["sudo", "systemctl", "start", "Greyrose.service"])
-subprocess.run(["sudo", "systemctl ", "enable", "Greyrose.service"])
+    #reloading changes based on conf table
+    subprocess.run(["sudo", "nft", "nftables.conf"])
 
-#get splunk forwarder off the internet
-print("Feting splunk forwarder off the internet...")
-subprocess.run(["wget", "-splunkforwarder.tgz", "forwarders[server]"])
-subprocess.run(["tar", "-xvzf", "splunkforwarder.tgz", "-C", "/opt/"])
-logging.debug("Fetched splunk forwarder off the internet")
+    #create service
+    subprocess.run(["cp", "Greyrose.service", "etc/systemd/system/Greyrose.service"])
+    subprocess.run(["sudo", "systemctl", "daemon-reload"])
+    subprocess.run(["sudo", "systemctl", "start", "Greyrose.service"])
+    subprocess.run(["sudo", "systemctl ", "enable", "Greyrose.service"])
 
-#start splunk forwarder
-print("Starting Splunk...")
-subprocess.run(["opt/splunkforwarder/bin/splunk", "start", "--accept-license"])
-logging.debug("Started Splunk")
-splunk = input("What Is The Splunk Ip Address?: ")
-port = input("What is the Splunk Port")
-forward_server = f"{splunk}:{port}"
+def act_III():
+    #get splunk forwarder off the internet
+    print("Fetching splunk forwarder off the internet...")
+    print(f"Forwarder Name: {forwarders[server]}")
+    subprocess.run(["sudo", "wget", "-O", "/opt/splunkforwarder-10.0.3-adbac1c8811c-linux-amd64.deb", forwarders[server]])
+    subprocess.run(["sudo", "dpkg", "-i", "splunkforwarder-10.0.3-adbac1c8811c-linux-amd64.deb"])
+    logging.debug("Fetched splunk forwarder off the internet")
 
-#add monitors
-print("Adding Monitors...")
-subprocess.run(["opt/splunkforwarder/bin/splunk", "add", "forward-server", "forward_server"])
-subprocess.run(["opt/splunkforwarder/bin/splunk", "add", "monitor", "/var/log"])
-subprocess.run(["opt/splunkforwarder/bin/splunk", "add", "monitor", "/etc/crontab"])
-subprocess.run(["opt/splunkforwarder/bin/splunk", "add", "monitor", "/etc/passwd"])
-subprocess.run(["opt/splunkforwarder/bin/splunk", "add", "monitor", "/etc/systemd/system"])
-subprocess.run(["opt/splunkforwarder/bin/splunk", "add", "monitor", "/usr/lib/systemd/system"])
-subprocess.run(["opt/splunkforwarder/bin/splunk", "enable", "boot-start"])
-logging.debug("Added new monitors to splunk")
+    #start splunk forwarder
+    #side note: Splunk will prompt you for an administrator username
+    print("Starting Splunk...")
+    subprocess.run(["sudo", "/opt/splunkforwarder/bin/splunk", "start", "--accept-license"])
+    logging.debug("Started Splunk")
+    splunk = input("What Is The Splunk Ip Address?: ")
+    port = input("What is the Splunk Port: ")
+    forward_server = f"{splunk}:{port}"
 
-#Add firewall to sbin
-print("Adding firewall command")
-subprocess.run(["sudo", "cp", "firewall", "/usr/local/sbin/"])
-subprocess.run(["sudo", "chown", "root:sysadmin", "/usr/local/sbin/firewall"])
-subprocess.run(["sudo", "chmod", "700", "/usr/local/sbin/firewall"])
-logging.debug("Firewall command set")
+    #add monitors
+    print("Adding Monitors...")
+    subprocess.run(["/opt/splunkforwarder/bin/splunk", "add", "forward-server", "forward_server"])
+    subprocess.run(["/opt/splunkforwarder/bin/splunk", "add", "monitor", "/var/log"])
+    subprocess.run(["/opt/splunkforwarder/bin/splunk", "add", "monitor", "/etc/crontab"])
+    subprocess.run(["/opt/splunkforwarder/bin/splunk", "add", "monitor", "/etc/passwd"])
+    subprocess.run(["/opt/splunkforwarder/bin/splunk", "add", "monitor", "/etc/systemd/system"])
+    subprocess.run(["/opt/splunkforwarder/bin/splunk", "add", "monitor", "/usr/lib/systemd/system"])
+    subprocess.run(["/opt/splunkforwarder/bin/splunk", "enable", "boot-start"])
+    logging.debug("Added new monitors to splunk")
 
-#move quarentine to root directory
-subprocess.run(["mv", "quarantine", "/root/quarantine"])
+def epilogue():
+    #change the permission of every file in the directory
+    print("Applying permissions...")
+    subprocess.run(["sudo", "chmod", "700", "setup.py"])
+    subprocess.run(["sudo", "chmod", "700", "start.py"])
+    subprocess.run(["sudo", "chmod", "700", "tracker.py"])
+    logging.debug("Applying permissions to files inside Greyrose Directory")
+
+    #Add firewall to sbin
+    print("Adding firewall command")
+    subprocess.run(["sudo", "cp", "firewall", "/usr/local/sbin/"])
+    subprocess.run(["sudo", "chown", "root:sysadmin", "/usr/local/sbin/firewall"])
+    subprocess.run(["sudo", "chmod", "700", "/usr/local/sbin/firewall"])
+    logging.debug("Firewall command set")
+
+    #move quarentine to root directory
+    subprocess.run(["mv", "quarantine", "/root/quarantine"])
+
+act_III()
