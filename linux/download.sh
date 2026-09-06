90a85633afae933a011fe8bc5723508dc79f3686
Christian-K9 <cmkent2005@gmail.com>
2026-09-05 21:21:29 -0400

    hopefully this works

--- linux/download.sh
+++ linux/download.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 sudo apt-get install libmariadb-dev -y
-
+sleep 5
 sudo apt install build-essential -y
-
+sleep 5
 sudo apt install python3-dev -y
-
+sleep 5
 sudo apt install python3-venv -y
-
+sleep 5
 sudo apt install mariadb-server -y
-
+sleep 5
 sudo apt install mariadb-client -y
+sleep 5
