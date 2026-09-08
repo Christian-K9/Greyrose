768a7cc0b190ee54f57019214d12a0966874198d
Christian-K9 <164112138+Christian-K9@users.noreply.github.com>
2026-09-07 20:09:04 -0400

    Update setup.py

--- linux/setup.py
+++ linux/setup.py
@@ -98,7 +98,7 @@ def act_III():
     subprocess.run(["sudo", "systemctl", "enable", "mariadb"])
     subprocess.run(["sudo", "systemctl", "start", "mariadb"])
     location = subprocess.run(["pwd"], capture_output=True, text=True)
-    result = f"{location.stdout}/ccdc_venv/bin python3"
+    result = f"{location.stdout}/ccdc_venv/bin python3'"
     subprocess.run(["sudo", "sed", "-i", "'1i", result, "firewall"])
     subprocess.run(["sudo", "cp", "firewall", "/usr/local/bin/firewall"])
     subprocess.run(["sudo", "chmod", "755", "/usr/local/bin/firewall"])
