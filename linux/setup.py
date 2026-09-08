4786d4ae7d17779b89096c490e092df1a9650f86
Christian-K9 <164112138+Christian-K9@users.noreply.github.com>
2026-09-07 20:33:21 -0400

    Update setup.py

--- linux/setup.py
+++ linux/setup.py
@@ -97,8 +97,8 @@ def act_III():
     #activate mariadb
     subprocess.run(["sudo", "systemctl", "enable", "mariadb"])
     subprocess.run(["sudo", "systemctl", "start", "mariadb"])
-    location = subprocess.run(["pwd"], capture_output=True, text=True)
-    shebang = f"{location.stdout}/ccdc_venv/bin python3"
+    location = os.path.join(os.getcwd(), "ccdc_venv", "bin", "python3")
+    shebang = f"#!{location}"
     print(f"shebang: {shebang}")
     subprocess.run(["sudo", "sed", "-i", f"1i {shebang}", "firewall"])
     subprocess.run(["sudo", "cp", "firewall", "/usr/local/bin/firewall"])
