2f4fe611b25601ff74cbf1ec1285d7b434692647
Christian-K9 <164112138+Christian-K9@users.noreply.github.com>
2026-09-09 17:08:24 -0400

    Update connectors.sql

--- linux/connectors.sql
+++ linux/connectors.sql
@@ -14,12 +14,16 @@ CREATE TABLE IF NOT EXISTS accepted_ports (
     id INT AUTO_INCREMENT PRIMARY KEY,
     port INT,
     time_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    connection_type varchar(3)
+    transmission_type varchar(5)
 );
 
 CREATE TABLE IF NOT EXISTS blocked_ports (
     id INT AUTO_INCREMENT PRIMARY KEY,
     port INT,
     time_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    connection_type varchar(3)
+    transmission_type varchar(5)
 );
 
 CREATE TABLE IF NOT EXISTS allowed_services (
