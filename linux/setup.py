ff4cdc9148f159b8b3776fba9f3d1cca91cee4df
Christian-K9 <164112138+Christian-K9@users.noreply.github.com>
2026-09-13 14:11:44 -0400

    Update setup.py

--- linux/setup.py
+++ linux/setup.py
@@ -136,11 +136,11 @@ def act_II():
 
     logging.debug("Mariadb service started")
 
-    location = os.path.join(os.getcwd(), log_name)
+    log_location = os.path.join(os.getcwd(), log_name)
                             
     lines = [
         f"databasename=Greyrose_DB\n",
-        f"log_name={location}\n",
+        f"log_name={log_location}\n",
         f"username={username}\n",
         f"password={password}\n"
     ]
