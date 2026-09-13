aab7f67fdecbb6224c74273ebc632fda6432d713
Christian-K9 <164112138+Christian-K9@users.noreply.github.com>
2026-09-13 12:28:34 -0400

    Update tracker.py

--- linux/tracker.py
+++ linux/tracker.py
@@ -117,7 +117,7 @@ def checkUsers():
                 os.system("userdel " + username)
                 logging.error(f"User {username} found on machine with unusual id")
                 logging.info(f"User {username} removed from machine")
-            elif (int(user_id) >= 1000):
+            elif (int(user_id) > 1000):
                 os.system("userdel " + userSplit[0])
                 logging.error(f"User {username} found on machine with unusual id")
                 logging.info(f"User {username} removed from machine")
