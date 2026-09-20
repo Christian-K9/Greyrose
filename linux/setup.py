94d6fc1a7ab56ed397d05a8508494640d479bf57
Christian-K9 <164112138+Christian-K9@users.noreply.github.com>
2026-09-20 19:39:52 -0400

    Update setup.py

--- linux/setup.py
+++ linux/setup.py
@@ -66,11 +66,12 @@ def exposition():
     print(f"manager: {manager}")
     time.sleep(5)
     time.sleep(0.1)
-    libraries = ["python3-dev", "python3-venv", "python3-pip", "mariadb-server"]
+    libraries = ["python3-dev", "python3-pip", "mariadb-server"]
     if server == "ubuntu":
         libraries.append("libmariadb-dev")
         libraries.append("build-essential")
         libraries.append("mariadb-client")
+        libraries.append("python3-venv")
     else:
         libraries.append("mariadb-connector-c-devel")
         libraries.append("'Development Tools'")
