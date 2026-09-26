316dd34791409d7de79fbd040713f93f753456ca
Christian-K9 <164112138+Christian-K9@users.noreply.github.com>
2026-09-26 02:25:19 -0400

    Revert "Update setup.py"
    This reverts commit dc1610208b029af9941fb73bf5afd705a215cccb.

--- linux/setup.py
+++ linux/setup.py
@@ -121,7 +121,7 @@ def try_again(library, manager):
     time.sleep(3)
     try:
         if server == "fedora":
-            subprocess.run(["sudo", manager, "install", library, "-y", "--allowerasing"])
+            subprocess.run(["sudo", manager, "install", library, "-y", "--allow-erasing"])
         else:
             subprocess.run(["sudo", manager, "install", library, "-y"])
 
